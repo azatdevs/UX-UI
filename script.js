b7b8dc871799fc97f60438c2339f7d77f54bf6bf
azatdevs <azatsultanbekovich03@gmail.com>
2023-11-27 09:07:58 +0600

    UX/UI

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-
-    const swiper = new Swiper(".mySwiper", {
-      spaceBetween: 30,
-      effect: "fade",
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
+const swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  effect: "fade",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
